afb2d9293c8dd298d6a330b19225ea80ad1a4b5b
hafifamudi <hafifcyber@gmail.com>
2021-07-01 21:41:37 +0800

    add : auth service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
+// Music Plugin
 const musics = require('./api/open-music');
 const MusicsService = require('./services/postgres/MusicsService');
 const MusicsValidator = require('./validator/musics');
 
+// Users Plugin
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const musicsService = new MusicsService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: musics,
-    options: {
-      service: musicsService,
-      validator: MusicsValidator,
+  await server.register([
+    {
+      plugin: musics,
+      options: {
+        service: musicsService,
+        validator: MusicsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
