001ce29bb0ddf66492c84d54b1fa1d547deb626d
hafifamudi <hafifcyber@gmail.com>
2021-07-01 23:34:15 +0800

    authentication implemented

--- migrations/1625146873490_create-table-authentications.js
+++ migrations/1625146873490_create-table-authentications.js
@@ -2,6 +2,15 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
-exports.down = pgm => {};
+exports.down = pgm => {
+ pgm.dropTable('authentications');
+};
